39c6f0ff12a9036860713673c3b28a9f2edf5ccc
Sebastian Espei <seblsebastian@aol.de>
2025-10-26 17:01:54 +0100

    Add snapshot metrics as prometheus metrics

--- snapshot/restic_collector.go
+++ snapshot/restic_collector.go
@@ -2,6 +2,7 @@ package snapshot
 
 import (
 	"os/exec"
+	"strings"
 
 	"github.com/prometheus/client_golang/prometheus"
 )
@@ -43,4 +44,40 @@ func (c *Collector) Collect(ch chan<- prometheus.Metric) {
 
 	totalSnapshotCount := getTotalSnapshotCount(groupData)
 	ch <- prometheus.MustNewConstMetric(snapshotCountTotalDesc, prometheus.GaugeValue, float64(totalSnapshotCount))
+
+	for _, group := range groupData {
+		hostname := group.GroupKey.Hostname
+		tags := strings.Join(group.GroupKey.Tags, ",")
+		_, snapshotCount := getSnapshotCountByGroup(group)
+		ch <- prometheus.MustNewConstMetric(
+			snapshotCountDesc,
+			prometheus.GaugeValue,
+			float64(snapshotCount),
+			hostname,
+			tags,
+		)
+
+		if snapshotCount > 0 {
+			_, metrics, err := getSnapshotMetricsByGroup(group)
+			if err != nil {
+				panic(err)
+			}
+
+			ch <- prometheus.MustNewConstMetric(lastSnapshotTimeDesc, prometheus.GaugeValue, float64(metrics.Time.Unix()), hostname, tags)
+			ch <- prometheus.MustNewConstMetric(lastSnapshotBackupStartDesc, prometheus.GaugeValue, float64(metrics.BackupStart.Unix()), hostname, tags)
+			ch <- prometheus.MustNewConstMetric(lastSnapshotBackupEndDesc, prometheus.GaugeValue, float64(metrics.BackupEnd.Unix()), hostname, tags)
+			ch <- prometheus.MustNewConstMetric(lastSnapshotFilesNewDesc, prometheus.GaugeValue, float64(metrics.FilesNew), hostname, tags)
+			ch <- prometheus.MustNewConstMetric(lastSnapshotFilesChangedDesc, prometheus.GaugeValue, float64(metrics.FilesChanged), hostname, tags)
+			ch <- prometheus.MustNewConstMetric(lastSnapshotFilesUnmodifiedDesc, prometheus.GaugeValue, float64(metrics.FilesUnmodified), hostname, tags)
+			ch <- prometheus.MustNewConstMetric(lastSnapshotDirsNewDesc, prometheus.GaugeValue, float64(metrics.DirsNew), hostname, tags)
+			ch <- prometheus.MustNewConstMetric(lastSnapshotDirsChangedDesc, prometheus.GaugeValue, float64(metrics.DirsChanged), hostname, tags)
+			ch <- prometheus.MustNewConstMetric(lastSnapshotDirsUnmodifiedDesc, prometheus.GaugeValue, float64(metrics.DirsUnmodified), hostname, tags)
+			ch <- prometheus.MustNewConstMetric(lastSnapshotDataBlobsDesc, prometheus.GaugeValue, float64(metrics.DataBlobs), hostname, tags)
+			ch <- prometheus.MustNewConstMetric(lastSnapshotTreeBlobsDesc, prometheus.GaugeValue, float64(metrics.TreeBlobs), hostname, tags)
+			ch <- prometheus.MustNewConstMetric(lastSnapshotDataAddedDesc, prometheus.GaugeValue, float64(metrics.DataAdded), hostname, tags)
+			ch <- prometheus.MustNewConstMetric(lastSnapshotDataAddedPackedDesc, prometheus.GaugeValue, float64(metrics.DataAddedPacked), hostname, tags)
+			ch <- prometheus.MustNewConstMetric(lastSnapshotTotalFilesProcessedDesc, prometheus.GaugeValue, float64(metrics.TotalFilesProcessed), hostname, tags)
+			ch <- prometheus.MustNewConstMetric(lastSnapshotTotalBytesProcessedDesc, prometheus.GaugeValue, float64(metrics.TotalBytesProcessed), hostname, tags)
+		}
+	}
 }
