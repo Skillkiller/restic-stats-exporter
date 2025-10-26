ca3e099c2ec8f4a8a2af78569f9880f8345927d0
Sebastian Espei <seblsebastian@aol.de>
2025-10-26 21:42:44 +0100

    Add unit tests for restic snapshot collector
    - Introduced tests for handling various restic exit codes.
    - Added tests for invalid JSON output cases.

--- snapshot/restic_collector_test.go
+++ snapshot/restic_collector_test.go
@@ -1,10 +1,14 @@
 package snapshot
 
 import (
+	"errors"
+	"fmt"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/testutil"
 )
 
 func TestCollector_Describe(t *testing.T) {
@@ -68,5 +72,117 @@ func TestCollector_Describe(t *testing.T) {
 		if !got[want] {
 			t.Fatalf("descriptor not sent: %s", want)
 		}
+	}
+}
+
+func TestCollector_Collect_VariousResticExitCodes(t *testing.T) {
+	type fields struct {
+		exitCode int
+	}
+
+	tests := []struct {
+		name   string
+		fields fields
+	}{
+		{
+			name: "exit code 1",
+			fields: fields{
+				exitCode: 1,
+			},
+		},
+		{
+			name: "exit code 10",
+			fields: fields{
+				exitCode: 1,
+			},
+		},
+		{
+			name: "exit code 12",
+			fields: fields{
+				exitCode: 1,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// fakeExec returns the specified exit code
+			fakeExec := func(exe string, args ...string) ([]byte, error, int) {
+				return []byte(``), errors.New("error for unit test"), tt.fields.exitCode
+			}
+
+			c := &Collector{
+				resticExecutablePath: "",
+				commandExecutor:      fakeExec,
+			}
+
+			reg := prometheus.NewRegistry()
+			if err := reg.Register(c); err != nil {
+				t.Fatalf("failed to register collector: %v", err)
+			}
+
+			expected := `
+# HELP restic_snapshot_exit_code Exit code of the list snapshots command. See restic exit codes, except 1684 for json output parsing errors: https://restic.readthedocs.io/en/stable/075_scripting.html#exit-codes
+# TYPE restic_snapshot_exit_code gauge
+restic_snapshot_exit_code %d
+`
+
+			err := testutil.CollectAndCompare(reg, strings.NewReader(fmt.Sprintf(expected, tt.fields.exitCode)))
+			if err != nil {
+				t.Fatalf("unexpected metrics output: %v", err)
+			}
+		})
+	}
+}
+
+func TestCollector_Collect_InvalidJsonOutput(t *testing.T) {
+	type fields struct {
+		json string
+	}
+
+	tests := []struct {
+		name   string
+		fields fields
+	}{
+		{
+			name: "invalid 1",
+			fields: fields{
+				json: "{{",
+			},
+		},
+		{
+			name: "invalid 2",
+			fields: fields{
+				json: "[{",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// fakeExec returns the specified JSON output as bytes
+			fakeExec := func(exe string, args ...string) ([]byte, error, int) {
+				return []byte(tt.fields.json), nil, 0
+			}
+
+			c := &Collector{
+				resticExecutablePath: "",
+				commandExecutor:      fakeExec,
+			}
+
+			reg := prometheus.NewRegistry()
+			if err := reg.Register(c); err != nil {
+				t.Fatalf("failed to register collector: %v", err)
+			}
+
+			expected := `
+# HELP restic_snapshot_exit_code Exit code of the list snapshots command. See restic exit codes, except 1684 for json output parsing errors: https://restic.readthedocs.io/en/stable/075_scripting.html#exit-codes
+# TYPE restic_snapshot_exit_code gauge
+restic_snapshot_exit_code 1684
+`
+
+			err := testutil.CollectAndCompare(reg, strings.NewReader(expected))
+			if err != nil {
+				t.Fatalf("unexpected metrics output: %v", err)
+			}
+		})
 	}
 }
